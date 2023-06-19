f8d42a53cd4debfbaf85057102a63f1fb25b333f
cosmosbigbang <cosmosbigbang1s@gmail.com>
2023-06-19 15:07:15 +0900

    first api call

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+);
